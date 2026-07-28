b8e94e1305ae7e9ff18df8e93c35cf53c48161d7
Shaurya Narain Singh <shauryanarainsingh@gmail.com>
2026-07-28 13:59:56 +0530

    Time: 15 ms (95.82%), Space: 73.7 MB (44.61%)

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
+        // count sort. 
+
         string first_half = s.substr(0, s.size()/2);
-        sort(first_half.begin(), first_half.end());
-        
-        string second_half = first_half;
+
+        vector<int> freq(26);
+        for(char c:first_half) freq[c-'a']++;
+
+        vector<int> position = {0}; 
+        for(int i:freq) position.push_back(position.back() + i);
+
+        string sorted_first_half = first_half;
+        int pos;
+        for(char c:first_half){
+            pos = position[c-'a']++;
+            sorted_first_half[pos] = c;
+        }
+
+        string second_half = sorted_first_half;
         reverse(second_half.begin(), second_half.end());
 
-        if(s.size()%2 == 0) return first_half + second_half;
-        else return first_half + s[s.size()/2] + second_half;
+        if(s.size()%2 == 0) return sorted_first_half + second_half;
+        else return sorted_first_half + s[s.size()/2] + second_half;
         return "";
     }
 };
