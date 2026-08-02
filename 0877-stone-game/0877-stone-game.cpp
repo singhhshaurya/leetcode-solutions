d284c105d04a7fe7f49c0f913d597339092d2a7a
Shaurya Narain Singh <shauryanarainsingh@gmail.com>
2026-08-02 22:12:25 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.24%)

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    int dp(int start, int end, bool alex, vector<int>& piles){
-        if(end < start) return 0;
-        if(memo[start][end] != -INT_MAX) return memo[start][end];
-
-        int m = alex ? 1 : -1;
-
-        int ans = m*piles[start] + dp(start+1, end, !alex, piles);
-        ans = max(ans, m*piles[end] + dp(start, end-1, !alex, piles));
-        memo[start][end] = ans;
-        return ans;
-    }
     bool stoneGame(vector<int>& piles) {
-        // dp hai bas bc. bekar.
-        int n = piles.size();
-        memo.assign(n, vector<int>(n, -INT_MAX));
-        int a = dp(0, n-1, true, piles);
-        return a > 0;
-
+        return 1;
     }
 };
