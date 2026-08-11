f803d2b52ff316b85377139ef1b4952f9c1d548c
Shaurya Narain Singh <shauryanarainsingh@gmail.com>
2026-08-11 17:56:49 +0530

    Time: 526 ms (14.4%), Space: 43.8 MB (5.41%)

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -2,25 +2,24 @@ class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         # dp hi hai bc. ek aur factor aa jayega 'm' ka. initally its 1.
         # to wait khelna to hai hame but subtract nahi karna ab right??
-        memo = {}
         n = len(piles)
+        memo = [[[[-1, -1] for i in range(2)] for i in range(n+1)] for i in range(n)]
+
         def dp(index, m, alex=True):
             if index >= n: return 0
-            if (index, m, alex) in memo: return memo[(index, m, alex)][0]
+            if memo[index][m][alex] != [-1, -1]: return memo[index][m][alex][0]
 
             ans = -inf
             jump = 0
             curr_pile = 0
-            for i in range(2*m):
-                if index+i >= n:
-                    break
+            for i in range(min(2*m, n-index)):
                 curr_pile += piles[index+i]
                 ans2 = curr_pile - dp(index+i+1, max(i+1, m), not alex)
                 if ans2 > ans:
                     jump = i+1
                     ans = ans2
             
-            memo[(index, m, alex)] = (ans, jump)
+            memo[index][m][alex] = (ans, jump)
             return ans
 
         dp(0, 1, True)
@@ -32,8 +31,8 @@ class Solution:
         #     print(i, memo[i])
 
         while(ind < len(piles)):
-            ans += memo[(ind, m, alex)][0]
-            jumps = memo[(ind, m, alex)][1]
+            ans += memo[ind][m][alex][0]
+            jumps = memo[ind][m][alex][1]
             ind += jumps
             m = max(m, jumps)
             alex = not alex
